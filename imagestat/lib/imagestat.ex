cfef3030aa9118ae695ab0eb723261057f525113
ValdoR Che <ingeekteam@gmail.com>
2018-11-05 20:00:53 +0100

    Intialising the first util project. Image processing lib stat

---
+++ imagestat/lib/imagestat.ex
@@ -0,0 +1,18 @@
+defmodule Imagestat do
+  @moduledoc """
+  Documentation for Imagestat.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Imagestat.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ imagestat/test/imagestat_test.exs
@@ -0,0 +1,8 @@
+defmodule ImagestatTest do
+  use ExUnit.Case
+  doctest Imagestat
+
+  test "greets the world" do
+    assert Imagestat.hello() == :world
+  end
+end
